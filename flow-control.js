e03c75a851c4792fe3cdd852ad21e29d4f5b1641
Matthew Langi <onelove8012005@gmail.com>
2020-07-28 17:12:56 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,10 @@
-function basicTeenager(age) {
-
+function basicTeenager(age) {if (age === '13-19'){
+return "You are a teenager!"
 }
-function teenager(age) {
-
+function teenager(age) {if (age === "13-19"){ return "You are a teenager!"}
+else if (false) {return "You are not a teenager!"
+  
+}
 }
 
 function ageChecker(age) {
